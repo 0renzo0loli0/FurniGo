9406b1e4e77691d6fe12adcb24882a8ca05cd578
RenzoLoli <renzololi1@hotmail.com>
2023-10-24 00:14:01 -0500

    !code init design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
+    // Base components
     AppComponent
+
+    // Custom imports
   ],
   imports: [
+    // Helper imports
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+    // material imports
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatFormFieldModule, // !TODO - REVISAR SI SE USA
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule, // !TODO - REVISAR SI SE USA
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
